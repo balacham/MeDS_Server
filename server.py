72a2ec8bcc763b4281d3da839ab78354806b3db2
Mahesh Balachander <balacham@github.com>
2023-02-26 17:47:05 -0500

    Mid term demo

--- server.py
+++ server.py
@@ -4,12 +4,13 @@
 
 import socket
 import threading
+import time
 
 SERVER_IP = socket.gethostbyname(socket.gethostname())  # local ip of the machine
 SERVER_PORT = 5000  # arbitrary registered port (below 1024 are system ports, cannot use)
 FORMAT = 'utf-8'
-DISCONNECTION = "!DISCONNECT!"
-HEADER = 64
+DISCONNECTION = "/DISCONNECT/"
+HEADER = 4
 ACK = "MESSAGE RECEIVED"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -25,25 +26,77 @@ def server_start():
         conn, addr = server.accept()
         thread = threading.Thread(target=client_side, args = (conn, addr))
         thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
+flag = 0
+message = ""
+y = threading.Lock()
 
 # method to handle incoming client, runs in its own thread
 def client_side(conn, addr):
+    global flag
+    global message
+    global y
     # conn = connection, addr = address
     print(f"[NEW CONNECTION] {addr} connected.")
 
     connected = True
+    tmp = conn.recv(HEADER).decode(FORMAT)
+    print(tmp)
+
     while connected:
-        message_len = conn.recv(HEADER).decode(FORMAT)
-        if message_len:
+        if tmp == "iapp":
+            message_len = conn.recv(HEADER).decode(FORMAT)
             message_len = int(message_len)
             message = conn.recv(message_len).decode(FORMAT)
+            print(message)
+
             if message == DISCONNECTION:
                 connected = False
+                break
+            y.acquire()
+            flag = 1
+            y.release()
+        elif tmp == "mc32":
+            if flag == 1:
+                # print("Input:")
+                # val = input()
+                # conn.send(val.encode(FORMAT))
+                conn.send(message.encode(FORMAT))
+                y.acquire()
+                flag = 0
+                y.release()
+                time.sleep(5)
 
-            print(f"[{addr}] {message}")
-            conn.send(ACK.encode(FORMAT))
+    # conn.send("3   ".encode(FORMAT))
+    # print("sending...")
+    # conn.send(val.encode(FORMAT))
+    # while connected:
+        # conn.send(val.encode(FORMAT))
+        # print("sent!")
+        # message_len = conn.recv(tmp)#.decode(FORMAT)
+        # print("raw:", message_len)
+        # message_len = message_len.decode(FORMAT)
+
+        # val = input()
+        # if (val == "exit"):
+        #     connected = False
+        # else:
+        #     tmp = int(val) 
+
+        # count += 1
+        # if message_len:
+        #     message_len = int(message_len)
+        #     print("int:", message_len)
+        #     message = conn.recv(message_len).decode(FORMAT)
+        #     if message == DISCONNECTION:
+        #         connected = False
+
+        #     print(f"[{addr}] {message}")
+        #     print(message)
+        #     # conn.send(ACK.encode(FORMAT))
+        #     connected = False
     conn.close()
+    print(f"{addr} connection closed")
 
 server_start()
